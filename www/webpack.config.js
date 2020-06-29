83f382b9a1e45d3d4fc05cc8b64491b8159aaed6
apricot2012 <haotiangong@hotmail.com>
2020-06-29 15:24:18 -0700

    Added Sourcemap Loader

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -18,5 +18,9 @@ module.exports = {
         loader: 'awesome-typescript-loader',
       },
    ],
+   preLoaders: [
+    // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+    { test: /\.js$/, loader: "source-map-loader" }
+   ]
   }
 };
