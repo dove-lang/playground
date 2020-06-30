a28679c6ebbc10b419a98c2d37176c8bcb5483ce
Qiwen Hua <qiwen.hua@mail.utoronto.ca>
2020-06-29 21:32:54 -0700

    Add simple editor div.

--- www/src/index.ts
+++ www/src/index.ts
@@ -1,3 +1,5 @@
+import * as monaco from 'monaco-editor';
+
 // Fix "WebAssembly is included in initial chunk" issue
 // https://github.com/rustwasm/rust-webpack-template/issues/43#issuecomment-426597176
 function start(wasm: typeof import('hello-wasm-pack')) {
@@ -11,4 +13,12 @@ async function load() {
     start(await import('hello-wasm-pack'));
 }
 
-load().then();
+load().then(() => {
+    const editorContainer = document.getElementById("main-editor");
+    if (editorContainer) {
+        monaco.editor.create(editorContainer, {
+            value: 'console.log("hey, boss")',
+            language: 'javascript'
+        })
+    }
+});
