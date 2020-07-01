5e330a82e8f43202859c390e9f57c87cfa13e51c
Qiwen Hua <qiwen.hua@mail.utoronto.ca>
2020-06-30 22:09:02 -0700

    Add settings.ts for constants.

---
+++ www/src/settings.ts
@@ -0,0 +1,13 @@
+export namespace Editor {
+    export const LANG_ID = "dove";
+
+    export const INITIAL_EDITOR_VAL =
+`for name in ("foo", "bar") {
+    print "Hello World! " + name
+}
+`;
+}
+
+export namespace General {
+    export const DEFAULT_DL_FILENAME = "dove-playground.dove";
+}
