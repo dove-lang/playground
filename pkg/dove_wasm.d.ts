f3ee26677b62cdc1e4ef45533a30aa644bbb1278
Qiwen Hua <hqw@huaqiwen.com>
2020-07-03 15:33:47 -0700

    include /pkg folder

---
+++ pkg/dove_wasm.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* Run the source and return the output as an array of strings.
+* @param {string} source
+* @returns {Array<string>}
+*/
+export function run(source: string): Array<string>;
