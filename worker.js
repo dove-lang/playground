335f612f7e5e0dda54e0b76711561f00c703a3a9
Qiwen Hua <hqw@huaqiwen.com>
2020-07-11 18:56:10 -0700

    Updates

--- worker.js
+++ worker.js
@@ -92,7 +92,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/dove_wasm_bg.wasm":"b64b04799b9516d33cc7"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/dove_wasm_bg.wasm":"64619175b97c0e480ac0"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
